0924b3da65348d8281dcb5454f57f4102be187f8
mdurfey <mdurfey@paypal.com>
2019-02-16 16:48:42 -0800

    update images and site config

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: "Michael Durfey's Blog", // Navigation and Site Title
+  siteTitleAlt: 'Michael Durfey', // Alternative Site title for SEO
+  siteUrl: 'https://www.michaeldurfey.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
