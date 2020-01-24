e1f4705663cbae5daa14c978d378d4126ea1715d
Michael Durfey <mdurfey@me.com>
2020-01-23 22:06:02 -0800

    chore(analytics): add in google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,5 +70,16 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-156916247-2`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
   ],
 };
